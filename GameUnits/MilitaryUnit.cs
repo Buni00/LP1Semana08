8735fe6a595f50276ecff542a0c983c6bf14f835
Buni00 <bruno_hrf@outlook.com>
2023-05-05 02:06:43 +0100

    Change class Unit

---
+++ GameUnits/MilitaryUnit.cs
@@ -0,0 +1,13 @@
+namespace GameUnits
+{
+    public class MilitaryUnit : Unit
+    {
+        public int AttackPower {get; }
+        public int XP {get; private set; }
+
+        public void Attack (Unit u)
+        {
+
+        }
+    }
+}
